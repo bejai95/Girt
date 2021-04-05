2fc2c7d2cb953a8c0da6a09971dfa7bd46b03d98
Bejai Cobbing <z5313120@unsw.edu.au>
2021-04-06 05:06:32 +1000

    give  Tue  6 Apr 05:06:32 AEST 2021: More test scripts

--- test03.sh
+++ test03.sh
@@ -1,14 +1,14 @@
 #!/bin/dash
 
-# test00
+# test03
 
-# Testing the usage of girt-init
+# Testing the usage of girt-commit with the -a option
 
 export PATH=$PATH:.
 
 ################################################################################
 # Tests for 2041 girt - these will write the expected output (for both stdout
-# and stderr) of the reference girt-rm script with the tests onto 1.txt
+# and stderr) of the reference girt-commit script with the tests onto 1.txt
 ################################################################################
 
 (
@@ -19,13 +19,44 @@ fi
 
 2041 girt-init
 
-# Insert tests here
+# Test on first commit when there are no files in the index, should output "nothing to commit"
+2041 girt-commit -a -m "hello"
+
+# Test on first commit when there are files in the index
+echo "12345" > a
+echo "12345" > b
+2041 girt-add a b
+2041 girt-commit -a -m "yeah na"
+
+# Test adding after first commit
+echo "12345" > c
+2041 girt-add c
+2041 girt-commit -a -m "yesssss"
+
+# Test when the files in the index get changed
+echo "Adding different stuff" >> b
+2041 girt-commit -a -m "na yeah"
+2041 girt-show 0:b
+2041 girt-show 2:b
+
+# Make sure c doesn't exist on first commit
+2041 girt-show 0:c
+
+# Test when there are no files changed, should output "nothing to commit"
+2041 girt-commit -a -m "yeah yeah"
+
+# Test after removing files from the current directory
+rm a b
+2041 girt-commit -a -m noooooooooo
+2041 girt-show 3:a
+2041 girt-rm c
+2041 girt-commit -a -m "Go Hawthorn"
 
 ) > 1.txt 2>&1
 
 ################################################################################
 # Tests for ./girt - these will write the output (for both stdout and stderr) of
-# my girt-rm script with the tests onto 2.txt
+# my girt-commit script with the tests onto 2.txt
 ################################################################################
 
 (
@@ -34,9 +65,40 @@ if [ -d .girt ]; then
     rm -r -f .girt
 fi
 
-2041 girt-init
+girt-init
 
-# Insert tests here
+# Test on first commit when there are no files in the index, should output "nothing to commit"
+girt-commit -a -m "hello"
+
+# Test on first commit when there are files in the index
+echo "12345" > a
+echo "12345" > b
+girt-add a b
+girt-commit -a -m "yeah na"
+
+# Test adding after first commit
+echo "12345" > c
+girt-add c
+girt-commit -a -m "yesssss"
+
+# Test when the files in the index get changed
+echo "Adding different stuff" >> b
+girt-commit -a -m "na yeah"
+girt-show 0:b
+girt-show 2:b
+
+# Make sure c doesn't exist on first commit
+girt-show 0:c
+
+# Test when there are no files changed, should output "nothing to commit"
+girt-commit -a -m "yeah yeah"
+
+# Test after removing files from the current directory
+rm a b
+girt-commit -a -m noooooooooo
+girt-show 3:a
+girt-rm c
+girt-commit -a -m "Go Hawthorn"
 
 ) > 2.txt 2>&1
 
@@ -48,7 +110,7 @@ fi
 echo "$(cat 2.txt | sed 's/.\///g')" > 2.txt
 
 if diff 1.txt 2.txt; then
-    echo "You have passed test00 successfully. Congratulations!"
+    echo "You have passed test03 successfully. Congratulations!"
 fi
 
 rm 1.txt 2.txt
--- test04.sh
+++ test04.sh
@@ -1,14 +1,14 @@
 #!/bin/dash
 
-# test00
+# test04
 
-# Testing the usage of girt-init
+# Testing the usage of girt-log
 
 export PATH=$PATH:.
 
 ################################################################################
 # Tests for 2041 girt - these will write the expected output (for both stdout
-# and stderr) of the reference girt-rm script with the tests onto 1.txt
+# and stderr) of the reference girt-log script with the tests onto 1.txt
 ################################################################################
 
 (
@@ -17,15 +17,36 @@ if [ -d .girt ]; then
     rm -r -f .girt
 fi
 
+# Try doing a girt-log before doing a girt-init, should produce error message
+2041 girt-log
+
 2041 girt-init
 
-# Insert tests here
+# Try doing a girt-log with command line arguments, should produce usage error
+2041 girt-log f
+2041 girt-log a b c
+
+# Make lots of commits and then use test girt-log with correct syntax
+touch a b c d e
+2041 girt-add a
+2041 girt-commit -m "This assignmnment is gonna be the death of me"
+2041 girt-log
+2041 girt-add b
+2041 girt-commit -m "it's such a grinddddddd"
+2041 girt-add c
+2041 girt-commit -m "la la la la"
+2041 girt-add d
+2041 girt-commit -m "I haven't slept in days"
+2041 girt-add e
+2041 girt-commit -m "another assignment due Wednesday"
+2041 girt-log
+2041 girt-rm a b c d e
 
 ) > 1.txt 2>&1
 
 ################################################################################
 # Tests for ./girt - these will write the output (for both stdout and stderr) of
-# my girt-rm script with the tests onto 2.txt
+# my girt-log script with the tests onto 2.txt
 ################################################################################
 
 (
@@ -34,9 +55,30 @@ if [ -d .girt ]; then
     rm -r -f .girt
 fi
 
-2041 girt-init
+# Try doing a girt-log before doing a girt-init, should produce error message
+girt-log
 
-# Insert tests here
+girt-init
+
+# Try doing a girt-log with command line arguments, should produce usage error
+girt-log f
+girt-log a b c
+
+# Make lots of commits and then use test girt-log with correct syntax
+touch a b c d e
+girt-add a
+girt-commit -m "This assignmnment is gonna be the death of me"
+girt-log
+girt-add b
+girt-commit -m "it's such a grinddddddd"
+girt-add c
+girt-commit -m "la la la la"
+girt-add d
+girt-commit -m "I haven't slept in days"
+girt-add e
+girt-commit -m "another assignment due Wednesday"
+girt-log
+girt-rm a b c d e
 
 ) > 2.txt 2>&1
 
@@ -48,7 +90,7 @@ fi
 echo "$(cat 2.txt | sed 's/.\///g')" > 2.txt
 
 if diff 1.txt 2.txt; then
-    echo "You have passed test00 successfully. Congratulations!"
+    echo "You have passed test04 successfully. Congratulations!"
 fi
 
 rm 1.txt 2.txt
